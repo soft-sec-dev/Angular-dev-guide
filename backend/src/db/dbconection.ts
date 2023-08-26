e0d7b9cd3c0eaf2c07752c8400a4626ff50b6916
jordanhuaman <jordanelcracken@gmail.com>
2023-08-26 06:52:40 -0500

    feat: hide URI on db.conection.ts

--- backend/src/db/dbconection.ts
+++ backend/src/db/dbconection.ts
@@ -2,7 +2,7 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 
 export class MongoConection{
   private URI = process.env.DB_URI as string
-  public client = new MongoClient('mongodb+srv://jordanhuaman:ty0lTnHqiRkQtYIV@cluster0.argvt6o.mongodb.net/?retryWrites=true&w=majority')
+  public client = new MongoClient(`${this.URI}`)
 
 
   private async run(){
