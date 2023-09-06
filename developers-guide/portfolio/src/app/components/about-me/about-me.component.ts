9af839af57385c35b6442037fe29323b2466d363
jordanhuaman <jordanelcracken@gmail.com>
2023-09-05 21:21:30 -0500

    feat: about me finish

--- developers-guide/portfolio/src/app/components/about-me/about-me.component.ts
+++ developers-guide/portfolio/src/app/components/about-me/about-me.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { trigger,state, animate, style, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.css']
+  styleUrls: ['./about-me.component.css'],
+  animations:[
+    trigger('fade', [
+      state('void', style({ opacity: 0 })), // Estado inicial (cuando no existe)
+      transition('void <=> *', animate(1000))
+    ])
+  ]
 })
-export class AboutMeComponent {
+export class AboutMeComponent implements OnInit {
+  textAnimated: string = 'Software engineering'
+  ArrayText: Array<string> = ["Software engineering", "Software Developer", "DevOps"]
+  currentCase = 0
 
+  ngOnInit(): void {
+    setInterval(() => {
+      this.NumberAleatory()
+    }, 3000)
+  }
+
+  NumberAleatory() {
+    const caseNumberSelecter = Math.floor(Math.random() * 3) + 1
+    if (this.currentCase === caseNumberSelecter - 1) {
+      this.NumberAleatory()
+    } else {
+      this.currentCase = caseNumberSelecter - 1
+      this.textAnimated = this.ArrayText[this.currentCase]
+    }
+
+  }
 }
